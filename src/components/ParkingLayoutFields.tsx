f025cae0727fe0fdd98d8625fa6d57dc3b2ae917
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2024-12-06 22:58:30 +0700

    chore(parking-layout): fix parking layout items render

--- src/components/ParkingLayoutFields.tsx
+++ src/components/ParkingLayoutFields.tsx
@@ -25,37 +25,41 @@ const ParkingLayoutFields: React.FC<CreateParkingLayoutProps> = ({
   dataFills,
   onSelect,
 }) => {
-  return [...new Array(total)].map((_, index) => {
-    const itemXPoint: number = index * itemWidth + x;
-    const itemYPoint: number = y;
-    const width: number = itemWidth;
-    const height: number = itemHeight;
-    const spotKey: string = `${keyItemPrefix}-${index}`;
-    const isFilledSpot: boolean = dataFills.includes(spotKey);
+  return (
+    <>
+      {[...new Array(total)].map((_, index) => {
+        const itemXPoint: number = index * itemWidth + x;
+        const itemYPoint: number = y;
+        const width: number = itemWidth;
+        const height: number = itemHeight;
+        const spotKey: string = `${keyItemPrefix}-${index}`;
+        const isFilledSpot: boolean = dataFills.includes(spotKey);
 
-    return (
-      <Group x={itemXPoint} y={itemYPoint}>
-        <Rect
-          key={index}
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          fill={isFilledSpot ? filledColor : emptyColor}
-          stroke='black'
-          strokeWidth={2}
-          onClick={() => onSelect?.(spotKey)}
-        />
-        <Text
-          text={String(index + 1)}
-          x={width / 2 - 12}
-          y={height / 2 - 12}
-          fontSize={24}
-          fill='black'
-        />
-      </Group>
-    );
-  });
+        return (
+          <Group key={index} x={itemXPoint} y={itemYPoint}>
+            <Rect
+              key={index}
+              x={0}
+              y={0}
+              width={width}
+              height={height}
+              fill={isFilledSpot ? filledColor : emptyColor}
+              stroke='black'
+              strokeWidth={2}
+              onClick={() => onSelect?.(spotKey)}
+            />
+            <Text
+              text={String(index + 1)}
+              x={width / 2 - 12}
+              y={height / 2 - 12}
+              fontSize={24}
+              fill='black'
+            />
+          </Group>
+        );
+      })}
+    </>
+  );
 };
 
 export default ParkingLayoutFields;
