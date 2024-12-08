1c46184156f505ed87c0e124c26806d9325d5408
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2024-12-08 12:54:47 +0700

    feat(parking-lot): enable drag and zoom in parking lot layout

--- src/components/ParkingLot/ParkingLot.tsx
+++ src/components/ParkingLot/ParkingLot.tsx
@@ -1,4 +1,6 @@
+import { useRef, useState } from "react";
 import { Stage, Layer, Text } from "react-konva";
+import Konva from "konva";
 import ParkingLayoutFields from "../ParkingLayoutFields/ParkingLayoutFields";
 import { useParkingLotContext } from "../../context/ParkingLotProvider";
 
@@ -12,13 +14,64 @@ interface ParkingLotProps {
 const ParkingLot: React.FC<ParkingLotProps> = ({ onItemSelect }) => {
   const { activeSessions } = useParkingLotContext();
 
+  const [scale, setScale] = useState(1);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const stageRef = useRef(null);
+
   const handleSpotClick = (id: string) => {
     onItemSelect(id);
   };
 
+  const handleWheel = (e: Konva.KonvaEventObject<WheelEvent>) => {
+    e.evt.preventDefault();
+    const stage: any = stageRef.current;
+
+    if (stage) {
+      const oldScale = stage.scaleX();
+      const pointer = stage.getPointerPosition();
+
+      // Zoom factor
+      const scaleBy = 1.1;
+      const newScale =
+        e.evt.deltaY > 0 ? oldScale / scaleBy : oldScale * scaleBy;
+
+      // Update position and scale
+      const mousePointTo = {
+        x: (pointer.x - stage.x()) / oldScale,
+        y: (pointer.y - stage.y()) / oldScale,
+      };
+
+      const newPos = {
+        x: pointer.x - mousePointTo.x * newScale,
+        y: pointer.y - mousePointTo.y * newScale,
+      };
+
+      setScale(newScale);
+      setPosition(newPos);
+    }
+  };
+
+  const handleDragMove = (e: any) => {
+    const newPos = { x: e.target.x(), y: e.target.y() };
+    setPosition(newPos);
+  };
+
   return (
-    <div className='w-full min-h-screen'>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+    <div className='overflow-hidden'>
+      <Stage
+        className='bg-gray-500'
+        width={window.innerWidth}
+        height={window.innerHeight}
+        draggable
+        onDragMove={handleDragMove}
+        drag
+        scaleX={scale}
+        scaleY={scale}
+        x={position.x}
+        y={position.y}
+        onWheel={handleWheel}
+        ref={stageRef}
+      >
         <Layer>
           <Text text='Denah Parkir' x={20} y={0} fontSize={24} fill='black' />
           <ParkingLayoutFields
